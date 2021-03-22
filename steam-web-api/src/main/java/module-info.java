70b0e187a7d6cf7f23e202f98e24475facece4ba
iTitus <iTitus@users.noreply.github.com>
2021-03-22 20:53:03 +0100

    fix build

--- steam-web-api/src/main/java/module-info.java
+++ steam-web-api/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module io.github.ititus.valve_tools.steam_web_api {
     requires java.net.http;
     requires jdk.crypto.ec; // this is required to talk to servers with an elliptic curve certificate
     requires jdk.crypto.cryptoki;
-    requires jdk.crypto.mscapi;
     requires io.github.ititus.commons;
     requires com.google.gson;
 
